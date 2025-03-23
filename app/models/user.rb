cb3a4eaf81c3c9f2f6c253f70380f9147008a8ae
Maxwell Sherwin <maxwellsherwin@proton.me>
2025-03-23 15:49:09 -0500

    Add basic user authentication

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email_address, presence: true, uniqueness: true
+  validates :password, presence: true, confirmation: true
+
+
+  has_many :sessions, dependent: :destroy
+  belongs_to :company, optional: true
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+end
