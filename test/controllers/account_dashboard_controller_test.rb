fe61f3fbae14c99b8879d7566784cb16cf1c7f3b
mackswell13 <maxsherwin1323@gmail.com>
2025-03-24 08:19:01 -0500

    Remove account dashboard test (for now)

--- test/controllers/account_dashboard_controller_test.rb
+++ test/controllers/account_dashboard_controller_test.rb
@@ -1,8 +1,4 @@
 require "test_helper"
 
 class AccountDashboardControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get account_dashboard_show_url
-    assert_response :success
-  end
 end
